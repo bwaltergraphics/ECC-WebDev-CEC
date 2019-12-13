fe587cf51bcbf9bd364b2c6be2e08b174b3eea0e
Bill Walter <bwaltergraphics@yahoo.com>
2019-12-13 12:25:49 -0500

    first Upload of wordpress files

---
+++ ECC-WebDev-CEC/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,700i|Solway:400,700&display=swap" rel="stylesheet">
+
+        <?php wp_enqueue_style( 'style', get_stylesheet_uri() );
+
+        wp_head();
+        ?>
+
+    </head>
+
+    <body>
---
+++ ECC-WebDev-CEC/home.php
@@ -0,0 +1,79 @@
+<?php 
+    get_header();
+?>
+
+<!-- Big Top Graphic -->
+    <div class="jumbo"> 
+        <div class="one-col-container">
+            <h2 class="title">SUNY Clean Energy Consortium</h2>
+        </div> 
+    </div>
+
+
+<!-- Intro Section -->
+<section>
+    <div class="one-col-container">
+
+        <?php
+        /* intro copy widget*/
+
+        ?>
+
+        <h3 class="intro-copy">The industry is made up of five subsectors supporting clean energy:</h3>
+            <div class="flex-container">
+               
+                <div class="flex-col-container">
+                        <img  src="images/manufacturing-icon.png" alt="manufacturing icon">
+                        <h4>Manufacturing</h4>
+                </div>
+
+                <div class="flex-col-container" >
+                     <img  src="images/construction-Icon.png" alt="manufacturing icon">
+                    <h4>Construction and Installation</h4>
+                </div>
+
+                <div class="flex-col-container">
+                    <img  src="images/distribution-Icon.png" alt="manufacturing icon">
+                    <h4>Energy Distribution</h4>
+                </div>
+
+                <div class="flex-col-container">
+                    <img  src="images/consulting-icon.png" alt="manufacturing icon">
+                    <h4>Professional Services and Consulting</h4>
+                </div>
+
+                <div class="flex-col-container">
+                    <img  src="images/policy-icon.png" alt="manufacturing icon">
+                    <h4>Public Policy and Advocacy</h4>
+                </div>
+
+            </div>
+</section>
+
+
+<!-- Green Bar Comments Section -->
+<section class="goal-area">
+    <div class="one-col-container">               
+        <?php
+        /* goal copy widget*/
+
+        ?>
+    </div>
+</section>
+
+
+
+<section>
+    <div class="one-col-container">
+        <?php 
+        /* logo gallery widget*/
+
+        ?>
+    </div>
+</section>
+
+
+
+<?php 
+    get_footer();
+?>
