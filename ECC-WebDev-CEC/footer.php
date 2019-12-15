394f330bb4cbb1c688b783d07f4987a10d1d2c0a
CKwan-hub <55776052+CKwan-hub@users.noreply.github.com>
2019-12-15 18:30:53 -0500

    Update 1 - CK
    initial update with functioning footer. Styled and working with WP admin.

--- ECC-WebDev-CEC/footer.php
+++ ECC-WebDev-CEC/footer.php
@@ -1,6 +1,22 @@
-        <footer>
-            &copy; <?php echo date('Y'); ?>
-        </footer>
+ 
+    <footer class="site-footer">
+                <nav class="site-nav">
+                    <?php
+                        $args = array(
+                                'theme_location' => 'footer'
+                            );
+
+                    ?>
+
+                    <?php wp_nav_menu( $args ); ?>
+
+                </nav>
+           <p><?php bloginfo('name'); ?> - &copy; <?php echo date('Y'); ?></p>
+
+           
+    </footer>
+    </div> <!--closer for container in header -->
+        <?php wp_footer(); ?> 
     </body>
 
 </html>
