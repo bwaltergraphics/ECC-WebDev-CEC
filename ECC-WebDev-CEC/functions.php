997c46c45c4ec862dd042ca9772b36e64b73bf01
CKwan-hub <55776052+CKwan-hub@users.noreply.github.com>
2019-12-15 18:37:14 -0500

    Update 1 - CK
    Three updates
    linked stylesheet
    registered nav menu to wp admin to work with custom menu
    added utility link for uploading custom logos

--- ECC-WebDev-CEC/functions.php
+++ ECC-WebDev-CEC/functions.php
@@ -1,2 +1,24 @@
+<?php
+/* Stylesheet linking function */
+function P3_resources() {
+    wp_enqueue_style('style', get_stylesheet_uri());
+}
+
+add_action('wp_enqueue_scripts', 'P3_resources' );
+/* Nav menu registered in WP admin */
+register_nav_menus(array(
+    'primary' => __( 'Primary Menu'),
+    'footer' => __( 'Footer Menu'),
+
+
+));
+/*logo support added for custom upload */
+add_theme_support( 'custom-logo', array(
+	'height'      => 100,
+	'width'       => 400,
+	'flex-height' => true,
+	'flex-width'  => true,
+	'header-text' => array( 'site-title', 'site-description' ),
+) );
 
 
