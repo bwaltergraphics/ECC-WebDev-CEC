9408df1a3dd484477b92dc8106a477493621a5cb
CKwan-hub <55776052+CKwan-hub@users.noreply.github.com>
2019-12-15 18:33:09 -0500

    Update 1 -CK
    update with working header and nav. 
    
    TO DO:
    media query and js for shrinking and mobile menu
    JS for logo shrink to top left on scroll

--- ECC-WebDev-CEC/header.php
+++ ECC-WebDev-CEC/header.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
-<html>
+<html <?php language_attributes(); ?>>
 
     <head>
-        <title></title>
+        <title><?php bloginfo('name'); ?></title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,700i|Solway:400,700&display=swap" rel="stylesheet">
 
@@ -13,4 +13,39 @@
 
     </head>
 
-    <body>
+    <body <?php body_class(); ?>>
+        <div class="container">
+
+                <!--site header-->
+
+                <header class="site-header">
+                <nav class="site-nav">
+                        <?php
+                        $args = array(
+                            'theme_location' => 'primary'
+                        );
+
+                        ?>
+
+
+                        <?php wp_nav_menu( $args ); ?>
+
+                    </nav>
+                <div class="header-logo">
+                       <?php 
+                        $custom_logo_id = get_theme_mod( 'custom_logo' );
+                        $image = wp_get_attachment_image_src( $custom_logo_id , 'full' );
+                    ?>
+                     <a href="<?php echo home_url(); ?>"><img src="<?php echo $image[0]; ?>" alt=""></a>
+                    <h1><?php bloginfo('name'); ?></h1>
+                    <hr class="title-break-line" />
+                    <div class="header-desc"><h3><?php bloginfo('description'); ?></h3><div>
+
+                    
+
+                </header><!--/site-header-->
+        
+    
+
+
+    
