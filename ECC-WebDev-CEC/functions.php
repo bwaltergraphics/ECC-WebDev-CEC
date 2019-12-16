45533d700c95a72493e1e29747649cb17e5e4ea1
CKwan-hub <55776052+CKwan-hub@users.noreply.github.com>
2019-12-16 18:30:17 -0500

    Update 2 - CK
    Update with mobile menu

--- ECC-WebDev-CEC/functions.php
+++ ECC-WebDev-CEC/functions.php
@@ -1,18 +1,18 @@
 <?php
-/* Stylesheet linking function */
+
 function P3_resources() {
     wp_enqueue_style('style', get_stylesheet_uri());
 }
 
 add_action('wp_enqueue_scripts', 'P3_resources' );
-/* Nav menu registered in WP admin */
+//Nav menu
 register_nav_menus(array(
     'primary' => __( 'Primary Menu'),
     'footer' => __( 'Footer Menu'),
 
 
 ));
-/*logo support added for custom upload */
+//logo support added 
 add_theme_support( 'custom-logo', array(
 	'height'      => 100,
 	'width'       => 400,
@@ -20,5 +20,14 @@ add_theme_support( 'custom-logo', array(
 	'flex-width'  => true,
 	'header-text' => array( 'site-title', 'site-description' ),
 ) );
-
-
+// register a mobile menu
+function wdm_register_mobile_menu() {
+    add_theme_support( 'nav-menus' );
+    register_nav_menus( array('mobile-menu' => __( 'Mobile Menu', 'wdm' )) );
+}
+add_action( 'init', 'wdm_register_mobile_menu' );
+// load the JS file
+function wdm_mm_toggle_scripts() {
+    wp_enqueue_script( 'wdm-mm-toggle', get_stylesheet_directory_uri() . '/js/mobile-menu-toggle.js', array('jquery') );
+}
+add_action( 'wp_enqueue_scripts', 'wdm_mm_toggle_scripts' );
