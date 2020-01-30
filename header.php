7fa935a4d8a803795e091a0fb09162167754d683
CKwan-hub <55776052+CKwan-hub@users.noreply.github.com>
2020-01-30 12:13:42 -0500

    Update mobile
    Menu class for logo, still pulls custom through wordpress. 
    Will not display on larger DOM width, will display:none
    Switches to normal logo class after breakpoint
    Left aligned to match left aligned content in menu. 
    Resized regardless of user submitted size. 
    commented out correctly

--- header.php
+++ header.php
@@ -13,86 +13,106 @@
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
         <script>
-            $(document).ready(function(){
+            /* calls for conditional to hide li if screen smaller than desktop size (800px) */
+                $(document).ready(function(){
+                    if ($(window).width() < 800){
+                        $('li').hide();
+                    }
+                
+            /* Mobile menu functionality */
                 $("button").click(function(){
-
-                    if($("button").text() == "☰"){
-                        $("button").text("🞬");
+                    if($("button").text() == '\u2716'){
+                        $("button").text('\u2630');
                         }else{
-                        $("button").text("☰");
+                        $("button").text('\u2716');
                         }
                         $("li").toggle("slow");
                         });
                         });
-                        //$(window).on("scroll", function() {
                         $(function(){
                         $('.site-header').data('size','big');
                 });
-            $(document).ready(function(){       
-                var scroll_pos = 0;
-                $(document).scroll(function() { 
-                    scroll_pos = $(this).scrollTop();
-                    if(scroll_pos > 100) {
-                        $(".site-header").css('background-color', '#d5effc80');
-                        $('.site-header').css("box-shadow","2px 2px 4px #000");
-                    } else {
-                        $(".site-header").css('background-color', '#d5effc');
-                        $('.site-header').css("box-shadow","");
-                    }
+            
+            /* Scroll position function to change transparency and add styling on scroll */
+                $(document).ready(function(){       
+                    var scroll_pos = 0;
+                    $(document).scroll(function() { 
+                        scroll_pos = $(this).scrollTop();
+                        if(scroll_pos > 100 && $(window).width() > 800) {
+                            $(".site-header").css('background-color', '#d5effc80');
+                            $('.site-header').css("box-shadow","2px 2px 4px #000");
+                        } else {
+                            $(".site-header").css('background-color', '#d5effc');
+                            $('.site-header').css("box-shadow","");
+                        }
+                    });
                 });
-            });
-            $(window).scroll(function(){
-                if($(document).scrollTop() > 0){
-                    if($('.site-header').data('size') == 'big'){
-                        $('.site-header').data('size','small');
-                        $('.site-header').stop().animate({
-                        height:'70px'
-                        },600);
-                        $('.site-header nav ul li').data('size','small'); //not working. targets data, not element.  May need to do transition to shrink 
-                        }
-                        }
-                    else {
-                        if($('.site-header').data('size') == 'small')
-                        {
-                        $('.site-header').data('size','big');
-                        $('.site-header').stop().animate({
-                        height:'150px'
-                        },50);
-                        }  
-                        }
+
+            /* Jquery calling for resizing based on scroll position */
+                $(window).scroll(function(){
+                    if($(document).scrollTop() > 0 && $(window).width() > 800){
+                        if($('.site-header').data('size') == 'big'){
+                            $('.site-header').data('size','small');
+                            $('.site-header').stop().animate({
+                            height:'70px'
+                            },600);
+                            $('.site-header nav ul li').data('size','small'); 
+                            }
+                            }
+                        else {
+                            if($('.site-header').data('size') == 'small')
+                            {
+                            $('.site-header').data('size','big');
+                            $('.site-header').stop().animate({
+                            height:'150px'
+                            },50);
+                            }  
+                            }
+                            }); 
+
+                /* Jquery for resizing main logo on scroll */
+                    $(document).scroll(function() {
+                        $('.header-logo img').css({width: $(this).scrollTop() > 90? "50%":"100%"});
                         });
-            $(document).scroll(function() {
-                $('.header-logo img').css({width: $(this).scrollTop() > 90? "50%":"100%"});
-                });
-               // $('.site-header').css({box-shadow: $(this).scrollTop() > 90? "rgba(7, 7, 7, 0.678) 2px 2px 4px;": "none"}); //not working
         </script>
     </head>
 
     <body <?php body_class(); ?>>
         <div class="container">
                 <!--site header-->
-                <header class="site-header">
-                    <!-- why won't this pull styling???? * fixed-->
-                <div class="nav-button" ><button> 🞬 </button></div>
-                <nav class="site-nav desktop-menu">
-                <div class="header-logo">
-                       <?php 
-                        $custom_logo_id = get_theme_mod( 'custom_logo' );
-                        $image = wp_get_attachment_image_src( $custom_logo_id , 'full' ); 
-                    ?>
-                     <a href="<?php echo home_url(); ?>"><img src="<?php echo $image[0]; ?>" alt=""></a>
+            <header class="site-header">
+                <!--mobile logo properties -->
+                <div class="mobile-logo-container">
+                 <!--mobile logo image -->  
+                    <div class="mobile-logo">
+                        <?php 
+                            $custom_logo_id = get_theme_mod( 'custom_logo' );
+                            $image = wp_get_attachment_image_src( $custom_logo_id , 'full' ); 
+                        ?>
+                        <a href="<?php echo home_url(); ?>"><img src="<?php echo $image[0]; ?>" alt=""></a>
+                    </div>
                 </div>
+                <!-- mobile nav buttons-->
+                <div class="nav-button" ><button> &#9776 </button></div>
+                <!-- desktop menu/nav content-->
+                <nav class="site-nav desktop-menu">
+                    <div class="header-logo">
+                        <?php 
+                            $custom_logo_id = get_theme_mod( 'custom_logo' );
+                            $image = wp_get_attachment_image_src( $custom_logo_id , 'full' ); 
+                        ?>
+                        <a href="<?php echo home_url(); ?>"><img src="<?php echo $image[0]; ?>" alt=""></a>
+                    </div>
                         <?php
                         $args = array(
                             'theme_location' => 'primary'
                         );
                         ?>
                         <?php wp_nav_menu( $args );
-                        /* The below code checks if a mobile-menu is set from the backend in the menu settings. If a menu has been set it will be displayed in the header. Or else, a menu has not been set then display a message.*/
+                /* The below code checks if a mobile-menu is set from the backend in the menu settings. If a menu has been set it will be displayed in the header. Or else, a menu has not been set then display a message.*/
                         ?>
-                        
-                    </nav>
-                    <nav><?php
+                </nav>
+                <nav><?php
                     if ( function_exists('has_nav_menu') && has_nav_menu('mobile-menu') ) {
                             wp_nav_menu( array(
                             'depth' => 6,
@@ -105,13 +125,9 @@
                             } else {
                             echo "<ul class='nav mobile-menu'> <font style='color:red'>Mobile Menu has not been set</font> </ul>";
                         } ?>
-                    </nav>
-                </header><!--/site-header-->
-                <!--<div class="name-desc-container">
-                    <div class="title-name"><h1><?php bloginfo('name'); ?></h1></div><br>
-                    <hr class="title-break-line" />
-                    <div class="header-desc"><h3><?php bloginfo('description'); ?></h3></div>
-                </div>  -->
+                </nav>
+            </header><!--/site-header-->
+              
 
 
     
